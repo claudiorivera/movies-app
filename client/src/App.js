8656eb51b9279b6f195b18cb5a762ee7997929bb
Claudio Rivera <me@claudiorivera.com>
2020-05-20 10:25:54 -0500

    Hard coded movies into MoviesList state

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Router } from "@reach/router";
-import { Box } from "@material-ui/core";
 import { MainAppBar } from "./components/MainAppBar";
 import { MoviesList } from "./components/MoviesList";
 
@@ -8,16 +7,9 @@ export const App = () => {
   return (
     <>
       <MainAppBar />
-      <Box
-        display="flex"
-        flexWrap="wrap"
-        justifyContent="center"
-        marginTop="20px"
-      >
-        <Router>
-          <MoviesList path="/" />
-        </Router>
-      </Box>
+      <Router>
+        <MoviesList path="/" />
+      </Router>
     </>
   );
 };
