128665adebdf9ed32aaf54843397077df9a0c585
Claudio Rivera <me@claudiorivera.com>
2020-05-20 11:37:47 -0500

    GET movie by id working

--- routes/movies.js
+++ routes/movies.js
@@ -12,4 +12,24 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Get movie by id
+router.get("/:id", async (req, res) => {
+  try {
+    const { rows } = await db.query(`
+    SELECT id, title, poster, description, genres
+      FROM   movies m
+      JOIN  (
+        SELECT movie.movie_id AS id, array_agg(g.name) AS genres
+        FROM   movies_genres movie
+        JOIN   genres g  ON g.id = movie.genre_id
+        GROUP  BY movie.movie_id
+        ) g USING (id)
+      WHERE id = ${req.params.id};
+    `);
+    res.status(200).send(rows[0]);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 module.exports = router;
