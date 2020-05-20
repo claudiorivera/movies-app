b0bce8e5ba0074b5f16086703004bd39d7c490d0
Claudio Rivera <me@claudiorivera.com>
2020-05-20 10:39:18 -0500

    Working with static data

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export const MovieDetails = () => {
+  const [detailsViewMovie, setDetailsViewMovie] = useState({});
+
   return <div>MovieDetails</div>;
 };
--- client/src/components/MoviesList.js
+++ client/src/components/MoviesList.js
@@ -103,7 +103,7 @@ export const MoviesList = () => {
         "Toy Story is a 1995 American computer-animated adventure comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. The feature-film directorial debut of John Lasseter, it was the first feature-length film to be entirely computer-animated, as well as the first feature film from Pixar. The screenplay was written by Joss Whedon, Andrew Stanton, Joel Cohen, and Alec Sokolow from a story by Lasseter, Stanton, Pete Docter, and Joe Ranft. The film features music by Randy Newman, and was executive-produced by Steve Jobs and Edwin Catmull. The film features the voices of Tom Hanks, Tim Allen, Don Rickles, Wallace Shawn, John Ratzenberger, Jim Varney, Annie Potts, R. Lee Ermey, John Morris, Laurie Metcalf, and Erik von Detten. Taking place in a world where anthropomorphic toys come to life when humans are not present, its plot focuses on the relationship between an old-fashioned pull-string cowboy doll named Woody and an astronaut action figure, Buzz Lightyear, as they evolve from rivals competing for the affections of their owner Andy Davis to friends who work together to be reunited with him after being separated.",
     },
   ]);
-  // const [detailsViewMovie, setDetailsViewMovie] = useState({});
+
   return (
     <Box display="flex" flexWrap="wrap" justifyContent="center">
       {allMovies.map((movie) => (
