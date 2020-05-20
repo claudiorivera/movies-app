77d90dacc76db1e7178340b3d9c4fdeec69105c4
Claudio Rivera <me@claudiorivera.com>
2020-05-20 11:53:36 -0500

    Movie object fetched correctly.

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const MovieDetails = ({ movieId }) => {
   const [movieDetails] = useState({ id: movieId });
+  useEffect(() => {
+    const getMovieDetails = async (id) => {
+      console.log(id);
+      const response = await fetch(`/movies/${id}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      const movie = await response.json();
+      console.log(movie);
+    };
+    getMovieDetails(Number(movieId));
+  }, [movieId]);
 
   return <div>{movieDetails.id}</div>;
 };
