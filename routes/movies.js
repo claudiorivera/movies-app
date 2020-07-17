f3d54c366f20c4b57112b17b69aa60a2a992d7db
Claudio Rivera <me@claudiorivera.com>
2020-07-17 14:45:17 -0500

    Order by title instead of id

--- routes/movies.js
+++ routes/movies.js
@@ -5,7 +5,9 @@ const router = new Router();
 // Get all movies
 router.get("/", async (req, res) => {
   try {
-    const { rows: movies } = await db.query("SELECT * FROM movies ORDER BY id");
+    const { rows: movies } = await db.query(
+      "SELECT * FROM movies ORDER BY title"
+    );
     res.status(200).send(movies);
   } catch (error) {
     res.status(400).send(error);
