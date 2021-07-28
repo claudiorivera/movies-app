f8bb8c3d274f7c771fd9de1acc8e0f879c345858
Claudio Rivera <me@claudiorivera.com>
2021-07-27 21:33:06 -0500

    🐛 Fix 404 routes

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use("/movies", movies);
 // Serve client/build if we're in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // Start server
